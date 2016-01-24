6707b90bf1f2c1ae2c023646d1928dede7e09eda
ekmartin <mail@ekmartin.com>
2016-01-24 02:24:46 +0100

    Add more facts, from http://www.animalplanet.com/pets/cat-facts/
    Add more tests and use 'const' where possible

--- test.js
+++ test.js
@@ -14,11 +14,23 @@ const test = require('ava');
  */
 
 test('random fact', t => {
-	let randomFact = catFacts.random();
+	const randomFact = catFacts.random();
 
 	t.true(allFacts.indexOf(randomFact) >= 0);
 });
 
 test('all facts', t => {
 	t.same(catFacts.all, allFacts);
+});
+
+test('no facts should have extranous whitespace', t => {
+	catFacts.all.forEach(fact => {
+		t.same(fact.trim(), fact);
+	});
+});
+
+test('no facts should be duplicate', t => {
+	const set = new Set(catFacts.all);
+
+	t.same(set.size, catFacts.all.length);
 });
